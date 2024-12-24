096c2994683723cbbc9999d32f1ee36c649581ac
hanatawfik8 <hannatawfik35@gmail.com>
2024-12-24 18:31:47 +0200

    Adjusting Showing and Hiding Password

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-let loader = document.querySelector('.loader')
+let loader = document.querySelector('.loader'),
+    icon = document.querySelector('i'),
+    passwordInput = document.querySelector('#password')
 
 load(displayNone);
+
+icon.addEventListener('click', function () {
+    if (icon.classList.contains('fa-eye')) {
+        icon.classList.replace('fa-eye', 'fa-eye-slash')
+        passwordInput.setAttribute('type', 'text')
+
+    }
+    else if (icon.classList.contains('fa-eye-slash')) {
+        icon.classList.replace('fa-eye-slash', 'fa-eye')
+        passwordInput.setAttribute('type', 'password')
+    }
+})
+
 
 function load(callback) {
     setInterval(() => {
